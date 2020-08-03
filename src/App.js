91af208fa407b6d02016991fe3c93d20641446df
Diego Cordoba <diegocordoba87@hotmail.com>
2020-08-03 19:52:03 -0400

    removed router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import {BrowserRouter as Router, Route} from "react-router-dom";
+
 import Home from './containers/Home';
 import NavBar from "./components/NavBar"
 
 function App() {
   return (
-    <Router>
+    <>
       <NavBar/>
-      <Route exact path="/employeetable" component={Home}/>
-    </Router>
-  );
-}
+      <Home/>
+    </>
+  )}
 
 export default App;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,9 +4,9 @@ import {Link} from "react-router-dom"
 const NavBar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <Link to= "/" className="navbar-brand">
+      <a to= "/" className="navbar-brand">
         Employee Finder
-      </Link>
+      </a>
     </nav>
   );
 };
